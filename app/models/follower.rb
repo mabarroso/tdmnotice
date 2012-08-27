e905d0cdc425909797a6ba0d45cf09d617485671
mabarroso <mabarroso@mabarroso.com>
2012-08-27 23:54:35 +0200

    no espanish scoope

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,6 +1,8 @@
 class Follower < ActiveRecord::Base
   attr_accessible :follow_request_sent, :following, :id, :lang, :notified_at, :screen_name, :time_zone, :utc_offset
 
+  scope :no_es, where('lang <> ? and notified_at is null', 'es')
+
   def self.not_exists?(ids)
     self.find(ids)
     false
